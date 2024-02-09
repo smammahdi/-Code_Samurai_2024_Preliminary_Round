a5883f4fd55a1eb881d1d523908c5059f6da2386
SMAM Mahdi <mahdithe8@gmail.com>
2024-02-09 16:37:10 +0600

    db credentials added

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'demo_database',
+    host: 'aws-0-ap-south-1.pooler.supabase.com',
+    user: 'postgres.pwmyzjakquansgufouyw',
+    password: '/VUUc25h_BQZmg5',
+    database: 'postgres',
     port: 5432
 });
 
